12381a72fc850545a4114c739aa62f160f3b5651
lokman-hosen <lokman@mcc.com.bd>
2026-08-17 18:20:18 +0600

    Pricing page updated

--- resources/views/layouts/partials/header-auth.blade.php
+++ resources/views/layouts/partials/header-auth.blade.php
@@ -30,7 +30,7 @@
         <div class="tw-absolute tw-top-3 md:tw-top-8 tw-right-4 md:tw-right-10 tw-flex tw-items-center tw-gap-4 md:tw-gap-10"
             style="text-align: left">
             @if (!($request->segment(1) == 'business' && $request->segment(2) == 'register') && $request->segment(1) != 'login')
-                <a class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
+                <a class="tw-text-black tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
                     href="{{ action([\App\Http\Controllers\Auth\LoginController::class, 'login']) }}@if (!empty(request()->lang)) {{ '?lang=' . request()->lang }} @endif">{{ __('business.sign_in') }}</a>
             @endif
             <!-- Register  -->
@@ -41,12 +41,12 @@
                     <!-- Register Url -->
                     @if (config('constants.allow_registration'))
                         <a href="{{route('business.getRegister')}}@if(!empty(request()->lang)){{'?lang='.request()->lang}}@endif"
-                            class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white">{{ __('business.register') }}
+                            class="tw-text-black tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white">{{ __('business.register') }}
                         </a>
 
                         <!-- pricing url -->
                         @if (Route::has('pricing') && config('app.env') != 'demo' && $request->segment(1) != 'pricing')
-                            <a class="tw-text-white tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
+                            <a class="tw-text-black tw-font-medium tw-text-sm md:tw-text-base hover:tw-text-white"
                                 href="{{ action([\Modules\Superadmin\Http\Controllers\PricingController::class, 'index']) }}">@lang('superadmin::lang.pricing')</a>
                         @endif
                     @endif
