6ef0357afb2543cb5ae255d809ef0d79a69f2289
lokman-hosen <lokman@aspiretss.com>
2026-09-05 11:14:51 +0600

    Pricing page localization added

--- Modules/Superadmin/Resources/views/pricing/index.blade.php
+++ Modules/Superadmin/Resources/views/pricing/index.blade.php
@@ -10,7 +10,7 @@
                 <div class="tw-flex tw-flex-col tw-items-center">
 
                     <div class="tw-flex tw-flex-col tw-gap-2 tw-text-center">
-                        <p class="pos-price-eyebrow">Pricing</p>
+{{--                        <p class="pos-price-eyebrow">Pricing</p>--}}
                         <h2 class="pos-price-heading tw-font-bold tw-text-3xl tw-text-white">@lang('superadmin::lang.pricing')</h2>
                         <h3 class="tw-text-sm tw-font-medium tw-text-white">
                             Choose your prefered {{ config('app.name', 'YaigoPos') }} pricing plan
