74032f2b3b2563030eb23cd2ce7583de46e6f18e
lokman-hosen <lokman.cse.pstu@gmail.com>
2026-09-13 12:29:20 +0600

    Package card updated

--- Modules/Superadmin/Http/Controllers/SubscriptionController.php
+++ Modules/Superadmin/Http/Controllers/SubscriptionController.php
@@ -934,8 +934,7 @@ class SubscriptionController extends BaseController
             $coupon_code = $request->input('coupon_code');
             $coupon = null;
             //$price = (float) $package->price;
-            $ivaRate = 21;
-            $price = (float) $package->price + ($package->price * $ivaRate / 100);;
+            $price = (float) $package->price + ($package->price * $package->vat / 100);;
 
             if (! empty($coupon_code)) {
                 $coupon = SuperadminCoupon::where('coupon_code', $coupon_code)->first();
